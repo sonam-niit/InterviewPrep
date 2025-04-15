5c0893d9ce44020db8393ec2b1c477b35336023d
sonam-niit <sonam.gravity@gmail.com>
2025-04-15 13:17:51 +0530

    practice day 2

---
+++ day2/MigratoryBirds.js
@@ -0,0 +1,10 @@
+function migratoryBirds(arr) {
+    // Write your code here
+
+}
+
+const result = migratoryBirds(arr);
+console.log(result + '\n');
+
+//Search on hacker rank algorithms
+//Easy set of Question
